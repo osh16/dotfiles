ca5d998860b1400e84a96aae9005fd18bd2f2f6f
oskar <oskarhoskuldsson@gmail.com>
2024-11-16 12:59:23 +0000

    sma skriftur

---
+++ .local/bin/install_software.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [[ lsb_release -a | grep "Ubuntu" ]]; then
+  # Basic tól 
+  sudo apt install -y git wget curl gcc build-essential unzip tar 
+
+  # Hipstera forrit
+  sudo apt install -y zsh neovim tmux fzf fd-find ripgrep htop neofetch
+
+  # Gnome dót 
+  sudo apt install -y gnome-tweaks gnome-browser-connector
+
+  # Annað
+  sudo apt install -y dotnet python3 python3-venv luarocks gh
+  sudo snap install -y code postman chromium brave
+fi
+
+if [[ ! -d "$HOME/.nvm" ]]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+fi
--- .local/bin/snap_search_packages.sh
+++ .local/bin/snap_search_packages.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo snap search "${1}" | awk '{$2=$3=$4=""; print $0}' | tail -n +2 | sed 's/    / - /g' | fzf --sync | xargs -r sudo snap install
+sudo snap search "${1}" | awk '{$2=$3=$4=""; print $0}' | tail -n +2 | sed 's/    / - /g' | fzf --sync | xargs -r sudo snap install -y
