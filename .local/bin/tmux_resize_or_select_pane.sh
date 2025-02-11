3dd8273fe7e1e28dcc077c4adb56b148d7b3a57d
oskar <oskarhoskuldsson@gmail.com>
2025-02-11 22:48:14 +0000

    docs: smá skýring á hvað tmux_resize skriftan gerir

--- .local/bin/tmux_resize_or_select_pane.sh
+++ .local/bin/tmux_resize_or_select_pane.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Þetta er basic virkni til að toggla hægra pane
+# Hægra pane er eiginlega alltaf notað sem terminal, og vinstra pane textaritill
+# 1. Ef við erum í vinstra pane, togglum hægra pane
+# 2. Ef við erum í hægra pane, færum til vinstri og maxímíserum það
+
 if [ "$(tmux display-message -p "#{pane_at_left}")" = "1" ]; then
   tmux select-pane -R
 elif [ "$(tmux display-message -p "#{pane_at_right}")" = "1" ]; then
