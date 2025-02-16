11ced5742e32788c0f56b287615bb17bc085df42
oskar <oskarhoskuldsson@gmail.com>
2025-02-16 15:16:29 +0000

    .........

--- .config/nvim/lua/plugins/cmdline.lua
+++ .config/nvim/lua/plugins/cmdline.lua
@@ -1,6 +1,13 @@
 return {
-  'VonHeikemen/fine-cmdline.nvim',
-  requires = {
-    {'MunifTanjim/nui.nvim'}
-  }
+--	"VonHeikemen/fine-cmdline.nvim",
+--	dependencies = {
+--		{ "MunifTanjim/nui.nvim" },
+--	},
+--	config = function()
+--		require("fine-cmdline").setup({
+--			position = "top", -- Move the `:` command-line to the top
+--		})
+--
+--		vim.api.nvim_set_keymap("n", ":", "<cmd>FineCmdline<CR>", { noremap = true })
+--	end,
 }
