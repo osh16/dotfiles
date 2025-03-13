db315752bfc5a723110cd892140115faf4df2b01
osh16 <oskarmassive@gmail.com>
2025-03-13 20:00:20 +0000

    feat: supermaven

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -53,7 +53,9 @@ return {
           end, { "i", "s" }),
         }),
 
+
         sources = cmp.config.sources({
+          { name = "supermaven", group_index = 2 },
           { name = "copilot",  group_index = 2 },
           { name = "nvim_lsp", group_index = 2 },
           { name = "luasnip",  group_index = 2 },
