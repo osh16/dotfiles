a0de386c839a20bc5c941f9447862c603543c27e
oskar <oskar@spektra.is>
2025-06-10 21:19:33 +0000

    ...

--- local/.local/bin/chromium_profile_picker.sh
+++ local/.local/bin/chromium_profile_picker.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 cat "$HOME/snap/chromium/common/chromium/Local State" | jq -r '.profile.info_cache | to_entries[] | "\(.key): \(.value.name)"' | fzf --sync | awk -F ":" '{print $1}' | xargs -I {} chromium --profile-directory="{}" --new-tab
