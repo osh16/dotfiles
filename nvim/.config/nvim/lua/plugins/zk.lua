37b2d0d84f3bc685c4e5a8d54075d7b3b5015cbe
oskar <oskarhoskuldsson@gmail.com>
2025-07-09 21:52:15 +0000

    ...

--- nvim/.config/nvim/lua/plugins/zk.lua
+++ nvim/.config/nvim/lua/plugins/zk.lua
@@ -5,6 +5,10 @@ return {
 			require("zk").setup({
         picker = "snacks_picker"
 			})
+      vim.keymap.set("n", "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", { desc = "Zk New Note" })
+      vim.keymap.set("n", "<leader>zf", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", { desc = "Zk Find Notes" })
+      vim.keymap.set("n", "<leader>zt", "<Cmd>ZkTags<CR>", { desc = "Zk Tags" })
+      vim.keymap.set("v", "<leader>zl", ":'<,'>ZkInsertLink<CR>", { desc = "Zk Insert Link" })
 		end,
 	},
 }
