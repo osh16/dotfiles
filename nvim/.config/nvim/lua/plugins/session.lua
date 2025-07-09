2dcd3c1a0c98a912d3683b6ceb761a20cfdad379
oskar <oskarhoskuldsson@gmail.com>
2025-07-09 21:07:21 +0000

    feat: no autosession command for vimwiki

--- nvim/.config/nvim/lua/plugins/session.lua
+++ nvim/.config/nvim/lua/plugins/session.lua
@@ -2,6 +2,9 @@ return {
 	"rmagatti/auto-session",
 	lazy = false,
 	config = function()
+    if vim.env.NO_AUTOSESSION == "1" then
+      return
+    end
 		require("auto-session").setup({
       --post_restore_cmds = { "NvimTreeOpen" } 
       post_restore_cmds = { "lua require('snacks').explorer()" }
