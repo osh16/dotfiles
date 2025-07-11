e361df715949d4c3770f51c8c22073b6960c89d9
oskar <oskarhoskuldsson@gmail.com>
2025-07-11 09:11:45 +0000

    fix: nvim-tmux-navigation latency issues

--- nvim/.config/nvim/lua/plugins/nvim-tmux-navigation.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -1,12 +1,18 @@
 return {
-  "alexghergh/nvim-tmux-navigation",
-  config = function()
-    require('nvim-tmux-navigation').setup({
-      disable_when_zoomed = true,
-    })
-    vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { noremap = true, silent = true })
-    vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { noremap = true, silent = true })
-    vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { noremap = true, silent = true })
-    vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { noremap = true, silent = true })
-  end,
+  "christoomey/vim-tmux-navigator",
+  lazy = false, -- ensure it's always loaded
 }
+--return {
+--  "alexghergh/nvim-tmux-navigation",
+--  config = function()
+--    require('nvim-tmux-navigation').setup({
+--      disable_when_zoomed = false,
+--    })
+--    vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { noremap = true, silent = true })
+--    vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { noremap = true, silent = true })
+--    vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { noremap = true, silent = true })
+--    vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { noremap = true, silent = true })
+--  end,
+--}
+
+
--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -37,10 +37,10 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- Navigation
-vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+--vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+--vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+--vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+--vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 -- Movement
 vim.keymap.set("n", "j", "gj")
