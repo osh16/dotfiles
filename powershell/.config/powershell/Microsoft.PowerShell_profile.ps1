7a7af726a0b5775475299575c07c2f592efe2dda
oskar <oskar@spektra.is>
2026-02-12 12:18:07 +0000

    ....

--- powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,7 @@
 Set-PSReadLineOption -EditMode Vi
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
+Set-PSReadlineKeyHandler -Chord Ctrl+n -Function NextSuggestion
+Set-PSReadlineKeyHandler -Chord Ctrl+p -Function PreviousSuggestion
+
 . $HOME/.config/powershell/extra_variables.ps1
